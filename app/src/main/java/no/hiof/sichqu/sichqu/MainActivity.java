10d1bdeef605231147983e9e7a02c19c174b78e9
le-Q <32103046+le-Q@users.noreply.github.com>
2018-10-16 15:24:55 +0200

    renset og kombinert

--- app/src/main/java/no/hiof/sichqu/sichqu/MainActivity.java
+++ app/src/main/java/no/hiof/sichqu/sichqu/MainActivity.java
@@ -1,48 +1,38 @@
 package no.hiof.sichqu.sichqu;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class MainActivity extends AppCompatActivity {
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-
-    List<Product> productList;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-
 import com.android.volley.AuthFailureError;
-import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.JsonRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-
 import org.json.JSONObject;
-
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import no.hiof.sichqu.sichqu.Products.Produkt;
-import no.hiof.sichqu.sichqu.Products.UPC_data;
+
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
     String iste = "Iste grønn te lime";
     String cider = "Grevens cider skogsbær";
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+
+    List<Product> productList;
 
 
     @Override
@@ -143,4 +133,5 @@ public class MainActivity extends AppCompatActivity {
         //request.setRetryPolicy(new DefaultRetryPolicy(15000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_MAX_RETRIES));
         queue.add(request);
     }
+    */
 }
