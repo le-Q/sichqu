bcedaaf0189f41a9d6e77b13a8039a2e8f1fa5d5
le-Q <32103046+le-Q@users.noreply.github.com>
2018-10-19 12:39:41 +0200

    flytta ting fra main til handleliste

--- app/src/main/java/no/hiof/sichqu/sichqu/MainActivity.java
+++ app/src/main/java/no/hiof/sichqu/sichqu/MainActivity.java
@@ -18,34 +18,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.SearchView;
-import android.widget.TextView;
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-import com.google.gson.Gson;
-import org.json.JSONObject;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import no.hiof.sichqu.sichqu.Products.Products;
-import no.hiof.sichqu.sichqu.Products.Produkt;
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button buttonRegister;
@@ -53,76 +25,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText editTextPassword;
     private TextView textViewSignin;
     private Button buttonSkip;
-
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
-
     private ProgressDialog progressDialog;
     private FirebaseAuth firebaseAuth;
-
-
-    TextView textView;
-    String iste = "Iste grønn te lime";
-    String cider = "Grevens cider skogsbær";
-
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    SearchView searchBar;
-
-    List<Products> productList;
-
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        productList = new ArrayList<>();
 
-        mRecyclerView = findViewById(R.id.recyleViewListe);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mRecyclerView.setHasFixedSize(true);
-
-        //productList.add(new Products("Melk"));
-        //productList.add(new Products("Salt"));
-
-        mAdapter = new ProductAdapter(this, productList);
-        mRecyclerView.setAdapter(mAdapter);
-        textView = findViewById(R.id.textView);
-
-        productList = new ArrayList<>();
-
-        mRecyclerView = findViewById(R.id.recyleViewListe);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mRecyclerView.setHasFixedSize(true);
-
-        mAdapter = new ProductAdapter(this, productList);
-        mRecyclerView.setAdapter(mAdapter);
-        textView = findViewById(R.id.textView);
-
-        productList = new ArrayList<>();
-
-        mRecyclerView = findViewById(R.id.recyleViewListe);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mRecyclerView.setHasFixedSize(true);
-
-        //productList.add(new Products("Melk"));
-        //productList.add(new Products("Salt"));
-
-        mAdapter = new ProductAdapter(this, productList);
-        mRecyclerView.setAdapter(mAdapter);
-        textView = findViewById(R.id.textView);
-
-        productList = new ArrayList<>();
-
-        mRecyclerView = findViewById(R.id.recyleViewListe);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mRecyclerView.setHasFixedSize(true);
-
-        mAdapter = new ProductAdapter(this, productList);
-        mRecyclerView.setAdapter(mAdapter);
-        textView = findViewById(R.id.textView);
 
         firebaseAuth = FirebaseAuth.getInstance();
 
@@ -218,102 +131,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.search_menu, menu);
-        MenuItem menuItem = menu.findItem(R.id.searchmenu);
-        final SearchView searchView = (SearchView)menuItem.getActionView();
 
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                getResponse(query);
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
-            }
-        });
-
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    public void hentAPI(View view) {
-        getResponse(cider);
-        //getName(iste);
-    }
-
-    private void getResponse(String produktNavn) {
-        String URL = "https://kolonial.no/api/v1/search/?q="+produktNavn;
-        RequestQueue queue = Volley.newRequestQueue(this);
-        JsonObjectRequest request = new JsonObjectRequest
-                (Request.Method.GET, URL, null, new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        Gson gson = new Gson();
-                        Produkt produkt = gson.fromJson(response.toString(), Produkt.class);
-                        productList.add(produkt.getProducts()[0]);
-                        Log.e("Check Error", response.toString());
-                        mAdapter = new ProductAdapter(MainActivity.this, productList);
-                        mRecyclerView.setAdapter(mAdapter);
-                        Log.e("Check Error", produkt.getProducts()[0].getImages()[0].getThumbnail().getUrl());
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.e("Check Error", "Error");
-                    }
-                }) {
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String> map = new HashMap<>();
-                map.put("Content-Type", "application/json");
-                map.put("Accept", "application/json");
-                map.put("User-Agent", "QuangLe_Test");
-                map.put("X-Client-Token", "Pcd4FnKoER3Bfk2trn38DHV8umdCYkteOlp55CH9q0PoCDa2xc");
-                return map;
-            }
-        };
-        //request.setRetryPolicy(new DefaultRetryPolicy(15000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_MAX_RETRIES));
-        queue.add(request);
-    }
-    /* SKU kode henter
-    private void getName(String skuKode) {
-        String URL = "https://api.upcdatabase.org/product/"+skuKode+"/9E29B3CE1CA7A534EF90DCEC796F94AD";
-        RequestQueue queue = Volley.newRequestQueue(this);
-        JsonObjectRequest request = new JsonObjectRequest
-                (Request.Method.GET, URL, null, new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        //textView.setText("Response : " + response.toString());
-                        Log.e("Check Error", response.toString());
-                        Gson gson = new Gson();
-                        UPC_data upc_produkt = gson.fromJson(response.toString(), UPC_data.class);
-                        Log.e("Check Error", upc_produkt.getUpcnumber());
-                        getResponse(upc_produkt.getTitle());
-
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.e("Check Error", "Error");
-                    }
-                }) {
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String> map = new HashMap<>();
-                map.put("Content-Type", "application/json");
-                map.put("Accept", "application/json");
-                map.put("X-Client-Token", "9E29B3CE1CA7A534EF90DCEC796F94AD");
-                return map;
-            }
-        };
-        //request.setRetryPolicy(new DefaultRetryPolicy(15000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_MAX_RETRIES));
-        queue.add(request);
-    }
-    */
 }
 
