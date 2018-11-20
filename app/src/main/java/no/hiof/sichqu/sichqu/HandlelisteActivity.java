fb438f7dbb337d3fe5b0e81a8e70f216a6748d9f
Unknown <quang.d.le@hiof.no>
2018-11-20 13:56:07 +0100

    fix

--- app/src/main/java/no/hiof/sichqu/sichqu/HandlelisteActivity.java
+++ app/src/main/java/no/hiof/sichqu/sichqu/HandlelisteActivity.java
@@ -184,11 +184,11 @@ public class HandlelisteActivity extends AppCompatActivity {
                 if (!lister.isEmpty())
                 databaseReference = firebaseDatabase.getReference("produkter").child(firebaseAuth.getUid()).child(lister.get(0));
                 Log.e("Handel", " Test -> " + lister.get(0) + testHandleliste + " " + databaseReference.toString());
-
+=======
                 if (!lister.isEmpty()) {
                     databaseReference = firebaseDatabase.getReference("produkter").child(firebaseAuth.getUid()).child(lister.get(0));
-                }
 
+                }
             }
 
             @Override
@@ -537,10 +537,12 @@ public class HandlelisteActivity extends AppCompatActivity {
 
                 if (!TextUtils.isEmpty(name)) {
                     addNewItem(product);
-                    databaseRead();
-
+<<<<<<< HEAD
                     Log.e("getUID", "Uid: " + user.getUid() + "***Liste: " + databaseReference.child(firebaseAuth.getUid()) + " -VS- ");
                     Toast.makeText(HandlelisteActivity.this, "Varen lagt til.. " + editName.getText().toString().trim(), Toast.LENGTH_LONG).show();
+
+=======
+>>>>>>> master
                 } else {
                     Toast.makeText(HandlelisteActivity.this, "Skriv inn navn på produkt", Toast.LENGTH_LONG).show();
                 }
